8837505bfd796d20f479655620520e42786358b7
beber92 <antoine.bertrand@student.ecp.fr>
2018-06-13 19:14:22 +0200

    map entity match

--- src/Entity/Match.php
+++ src/Entity/Match.php
@@ -1,9 +1,39 @@
-namespace Entity;
+<?php
 
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="match")
+ */
 class Match
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="array", length=2)
+     */
     private $players;
+
+    /**
+     * @ORM\Column(type="array", length=2)
+     */
     private $score;
+
+    /**
+     * @ORM\Column(type="array", length=2)
+     */
     private $teams;
+
+    /**
+     * @ORM\Column(type="date")
+     */
     private $date;
 }
