d8ed878e85e7ebf2a3e135e4e8afb1bb4c9b394b
beber92 <antoine.bertrand@student.ecp.fr>
2018-07-04 11:42:05 +0200

    optimize code in Player Controller

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -7,42 +7,13 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Entity\Player;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class PlayerController extends Controller
 {
     /**
-     * @Route("/add/player", name="add_player")
-     */
-    public function addPlayer(EntityManagerInterface $entityManager)
-    {
-      $request_body = json_decode(file_get_contents('php://input'));
-      $player = new Player();
-      $player->setName($request_body->player);
-      $player->setPlayed(0);
-      $player->setWins(0);
-      $player->setDraws(0);
-      $player->setLosses(0);
-      $player->setPoints(0);
-      $player->setGoaldiff(0);
-      $player->setFor(0);
-      $player->setAgainst(0);
-      $player->setTrophies([]);
-
-      $entityManager->persist($player);
-
-      $entityManager->flush();
-
-      $response = new JsonResponse(
-        'added player',
-        200,
-        array('access-control-allow-origin' => '*')
-      );
-
-      return $Response;
-    }
-
-    /**
-     * @Route("/fetch/players", name="fetch_players")
+     * @Route("/players", name="fetch_players")
+     * @Method({"GET"})
      */
     public function fetchPlayers()
     {
@@ -53,13 +24,13 @@ class PlayerController extends Controller
       $data = [];
 
       foreach($players as $player) {
-        array_push($data, [$player->getId() => $player->toJson()]);
+        array_push($data, [$player->playerToJson()]);
       };
 
       $response = new JsonResponse(
         $data,
         200,
-        array('accless-control-allow-origin' => '*')
+        array('access-control-allow-origin' => '*')
       );
 
       return $response;
