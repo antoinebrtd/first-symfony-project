70ddfe158a43953639e9eb444762c7ee7a9c9fc3
beber92 <antoine.bertrand@student.ecp.fr>
2018-07-12 16:56:17 +0200

    try to fetch striking

--- src/Controller/FetchMatchController.php
+++ src/Controller/FetchMatchController.php
@@ -34,7 +34,7 @@ class FetchMatchController extends Controller
       };
 
       $to_compare->setTimeZone(new DateTimeZone('Europe/Paris'));
-      //faire avec query builder
+
       $qb = $entityManager->createQueryBuilder();
       $qb->select('m')
         ->from('App\Entity\Match', 'm')
@@ -49,7 +49,50 @@ class FetchMatchController extends Controller
       $data = array_map(function($match){return $match->matchToJson();}, $recent_matches);
 
 
-      
+      $response = new JsonResponse(
+        $data,
+        200,
+        array('access-control-allow-origin' => '*')
+      );
+
+      return $response;
+    }
+
+    /**
+     * @Route("/matches", name="fetch_striking_matches")
+     * @Method({"GET"})
+     */
+    public function fetchStrikingMatches(Request $request, EntityManagerInterface $entityManager)
+    {
+      $value = $request->query->get('striking');
+      $qb = $entityManager->createQueryBuilder();
+      switch ($value) {
+        case 0:
+          $qb->select('m')
+            ->from('App\Entity\Match', 'm')
+            ->where('m.score[0] + m.score[1] > :goals')
+            ->orderBy('m.date', 'DESC')
+            ->setParameter('goals', 5);
+          break;
+        case 1:
+          $qb->select('m')
+            ->from('App\Entity\Match', 'm')
+            ->where('m.score[0] - m.score[1] > :goals')
+            ->orderBy('m.date', 'DESC')
+            ->setParameter('goals', 4);
+          break;
+        case 2:
+          $qb->select('m')
+            ->from('App\Entity\Match', 'm')
+            ->where('m.score[0] + m.score[1] < :goals')
+            ->orderBy('m.date', 'DESC')
+            ->setParameter('goals', 1);
+          break;
+      }
+
+      $striking_matches = $query->getResult();
+
+      $data = array_map(function($match){return $match->matchToJson();}, $striking_matches);
 
 
       $response = new JsonResponse(
